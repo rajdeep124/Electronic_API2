6d68e8b156014c168d7320847fadc0bd03537931
rajdeep124 <rajdeepkaursomal@gamil.com>
2021-02-10 12:34:53 +1300

    add details

---
+++ dbo.Product.data.sql
@@ -0,0 +1,9 @@
+﻿SET IDENTITY_INSERT [dbo].[Product] ON
+INSERT INTO [dbo].[Product] ([Id], [Product_Name], [Brand_Name], [Product_Category], [Price], [Discount]) VALUES (1, N'Mobile', N'Samsung', N'Mobile', CAST(300.00 AS Decimal(18, 2)), CAST(20.00 AS Decimal(18, 2)))
+INSERT INTO [dbo].[Product] ([Id], [Product_Name], [Brand_Name], [Product_Category], [Price], [Discount]) VALUES (2, N'Mobile', N'Samsung', N'Mobile', CAST(300.00 AS Decimal(18, 2)), CAST(20.00 AS Decimal(18, 2)))
+INSERT INTO [dbo].[Product] ([Id], [Product_Name], [Brand_Name], [Product_Category], [Price], [Discount]) VALUES (3, N'Mobile', N'Samsung', N'Mobile', CAST(300.00 AS Decimal(18, 2)), CAST(20.00 AS Decimal(18, 2)))
+INSERT INTO [dbo].[Product] ([Id], [Product_Name], [Brand_Name], [Product_Category], [Price], [Discount]) VALUES (4, N'Mobile', N'Samsung', N'Mobile', CAST(300.00 AS Decimal(18, 2)), CAST(20.00 AS Decimal(18, 2)))
+INSERT INTO [dbo].[Product] ([Id], [Product_Name], [Brand_Name], [Product_Category], [Price], [Discount]) VALUES (5, N'Mobile', N'Samsung', N'Mobile', CAST(300.00 AS Decimal(18, 2)), CAST(20.00 AS Decimal(18, 2)))
+INSERT INTO [dbo].[Product] ([Id], [Product_Name], [Brand_Name], [Product_Category], [Price], [Discount]) VALUES (6, N'Mobile', N'Samsung', N'Mobile', CAST(300.00 AS Decimal(18, 2)), CAST(20.00 AS Decimal(18, 2)))
+INSERT INTO [dbo].[Product] ([Id], [Product_Name], [Brand_Name], [Product_Category], [Price], [Discount]) VALUES (7, N'Mobile', N'Samsung', N'Mobile', CAST(300.00 AS Decimal(18, 2)), CAST(20.00 AS Decimal(18, 2)))
+SET IDENTITY_INSERT [dbo].[Product] OFF
